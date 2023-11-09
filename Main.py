36d7471943e029dec37435a72b849a6d4d10d65c
Mxytyu7 <130443471+Mxytyu7@users.noreply.github.com>
2023-11-09 14:24:08 +0100

    Update Main.py
    Made the code

--- Main.py
+++ Main.py
@@ -1,4 +1,19 @@
 import discord
 from discord.ext import commands
 
-print("logged in as ")
+# Create an instance of the bot
+bot = commands.Bot(command_prefix='!')
+
+# Event handler for when the bot is ready
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+# Define a simple command
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello, World!')
+
+# Run the bot with your token
+bot.run("YOUR_BOT_TOKEN")
+# replace YOUR_BOT_TOKEN with you bot s'token
