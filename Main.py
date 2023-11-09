16ac0f959cf5e014bca05793d20a5e7924f29806
Mxytyu7 <130443471+Mxytyu7@users.noreply.github.com>
2023-11-09 14:26:08 +0100

    Update Main.py
    Added ping command

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 
+# pip install discord.py
+
+
 # Create an instance of the bot
 bot = commands.Bot(command_prefix='!')
 
@@ -13,6 +16,12 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
     await ctx.send('Hello, World!')
+
+# Define a "ping" command
+@bot.command()
+async def ping(ctx):
+    latency = round(bot.latency * 1000)  # Convert latency to milliseconds
+    await ctx.send(f'Pong! Latency is {latency}ms')
 
 # Run the bot with your token
 bot.run("YOUR_BOT_TOKEN")
